32b03463eec78b8d426bfab78b8a7bda39dc9be8
JulianVerdirame <julian_verdirame@outlook.com>
2019-05-27 00:44:25 -0300

    Probando git(Julian)

--- TPObjetos/src/model/SistemaElectrico.java
+++ TPObjetos/src/model/SistemaElectrico.java
@@ -434,4 +434,5 @@ public class SistemaElectrico {
 		}
 	 
 	 //----------------------------------------Fin ABM Cliente---------------------------------------------------------
+		//Ignorar: Probando Git (Julian)
 }
