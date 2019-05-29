515429680338d07ed29f526d6b00e03bbc9e3a7d
Gabriel Nicolas Palazzo <gabriel.nicolas.palazzo.93@gmail.com>
2019-05-29 00:32:27 -0300

    En TarifaBaja pongo el agregar detalle y calcular total

--- TPObjetos/src/model/TarifaBaja.java
+++ TPObjetos/src/model/TarifaBaja.java
@@ -27,4 +27,46 @@ public class TarifaBaja extends Tarifa {
 		return "TarifaBaja [lstDetalle=" + lstDetalle + "]";
 	}
 
+	public void agregarDetalle(DetalleBaja detalle) {
+		
+		
+		DetalleBaja d1 = new DetalleBaja("Cargo Fijo", "$/mes", 0, 150, 32.82);
+		DetalleBaja d2 = new DetalleBaja("Cargo Variable", "$/kWh", 0, 150, 2.653);
+
+		DetalleBaja d3 = new DetalleBaja("Cargo Fijo", "$/mes", 151, 325, 56.11);
+		DetalleBaja d4 = new DetalleBaja("Cargo Variable", "$/kWh", 151, 325, 2.651);
+
+		DetalleBaja d5 = new DetalleBaja("Cargo Fijo", "$/mes", 326, 400, 91.73);
+		DetalleBaja d6 = new DetalleBaja("Cargo Variable", "$/kWh", 326, 400, 2.705);
+
+		DetalleBaja d7 = new DetalleBaja("Cargo Fijo", "$/mes", 401, 450, 107.34);
+		DetalleBaja d8 = new DetalleBaja("Cargo Variable", "$/kWh", 401, 450, 2.758);
+
+		DetalleBaja d9 = new DetalleBaja("Cargo Fijo", "$/mes", 450, 500, 161.66);
+		DetalleBaja d10 = new DetalleBaja("Cargo Variable", "$/kWh", 450, 500, 2.864);
+
+		DetalleBaja d11 = new DetalleBaja("Cargo Fijo", "$/mes", 501, 600, 316.00);
+		DetalleBaja d12 = new DetalleBaja("Cargo Variable", "$/kWh", 501, 600, 2.918);
+
+		DetalleBaja d13 = new DetalleBaja("Cargo Fijo", "$/mes", 601, 700, 850.24);
+		DetalleBaja d14 = new DetalleBaja("Cargo Variable", "$/kWh", 0, 150, 3.309);
+		
+		
+		lstDetalle.add(d1);
+		lstDetalle.add(d2);
+		lstDetalle.add(d3);
+		lstDetalle.add(d4);
+		lstDetalle.add(d5);
+		lstDetalle.add(d6);
+		lstDetalle.add(d7);
+		lstDetalle.add(d8);
+		lstDetalle.add(d9);
+		lstDetalle.add(d10);
+		lstDetalle.add(d11);
+		lstDetalle.add(d12);
+		lstDetalle.add(d13);
+		lstDetalle.add(d14);
+		
+	}
+	
 }
